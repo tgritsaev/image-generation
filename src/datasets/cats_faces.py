e67d8fb16755a503d582d506ddcd6a08f8ae1a79
GritsaevTim <timgri93@gmail.com>
2023-12-16 20:51:20 +0300

    fix

--- src/datasets/cats_faces.py
+++ src/datasets/cats_faces.py
@@ -16,5 +16,4 @@ class CatsFaces:
         return len(self.img_name)
 
     def __getitem__(self, index):
-        print(self.img_name[index])
         return torchvision.io.read_image(f"{self.root}/{self.img_name[index]}").to(torch.float32)
