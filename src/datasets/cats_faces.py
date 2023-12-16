22359f5f8b3045ac7f0f6a8a8c1b70b07e4401da
GritsaevTim <timgri93@gmail.com>
2023-12-16 20:51:15 +0300

    fix

--- src/datasets/cats_faces.py
+++ src/datasets/cats_faces.py
@@ -7,9 +7,9 @@ import torchvision
 class CatsFaces:
     def __init__(self, root, train: bool = True, limit: int = None):
         if train:
-            self.img_name = sorted(os.listdir(root))[:limit]
+            self.img_name = sorted(os.listdir(root))[: limit - 1]
         else:
-            self.img_name = sorted(os.listdir(root))[-limit:]
+            self.img_name = sorted(os.listdir(root))[-limit - 1 : -1]
         self.root = root
 
     def __len__(self):
