83f0b20eaf205778ed5a0d597060dfd33c62d416
GritsaevTim <timgri93@gmail.com>
2023-12-16 20:34:32 +0300

    fix

--- src/trainers/gan_trainer.py
+++ src/trainers/gan_trainer.py
@@ -128,7 +128,7 @@ class GANTrainer:
             for batch in tqdm(self.test_dataloader):
                 self.move_batch_to_device(batch)
                 bs = batch["img"].shape[0]
-                samples = self.g_model(batch["img"][:, last_idx : last_idx + bs].unsqueeze(-1).unsqueeze(-1))
+                samples = self.g_model(self.fixed_noize[:, last_idx : last_idx + bs].unsqueeze(-1).unsqueeze(-1))
 
                 real_imgs.append(batch["img"].detach().cpu().numpy())
                 constructed_imgs.append(samples.detach().cpu().numpy())
