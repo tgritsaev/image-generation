3d12a46aeb917eec97e942744b036a93ac83a8e0
GritsaevTim <timgri93@gmail.com>
2023-12-16 20:30:34 +0300

    fix

--- src/datasets/cats_faces.py
+++ src/datasets/cats_faces.py
@@ -7,12 +7,13 @@ import torchvision
 class CatsFaces:
     def __init__(self, root, train: bool = True, limit: int = None):
         if train:
-            self.img_path = sorted(os.listdir(root))[:limit]
+            self.img_name = sorted(os.listdir(root))[:limit]
         else:
-            self.img_path = sorted(os.listdir(root))[-limit:]
+            self.img_name = sorted(os.listdir(root))[-limit:]
+        self.root = root
 
     def __len__(self):
-        return len(self.img_path)
+        return len(self.img_name)
 
     def __getitem__(self, index):
-        return torchvision.io.read_image(self.img_path[index]).to(torch.float32)
+        return torchvision.io.read_image(f"{self.root}/{self.img_name[index]}").to(torch.float32)
