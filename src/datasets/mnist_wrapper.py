be7d49de0b1c7bfaa1741cfd4fb67ce3a5424422
GritsaevTim <timgri93@gmail.com>
2023-12-18 01:05:06 +0300

    fix

--- src/datasets/mnist_wrapper.py
+++ src/datasets/mnist_wrapper.py
@@ -1,12 +1,14 @@
 import torch
+import torchvision
 from torchvision.datasets import MNIST
 
 
 class MNISTWrapper(MNIST):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.resize = torchvision.transforms.Resize(size=(32, 32))
 
     def __getitem__(self, index: int):
         img, target = self.data[index], int(self.targets[index])
 
-        return img.unsqueeze(0).to(torch.float32), target
+        return self.resize(img.unsqueeze(0)).to(torch.float32), target
